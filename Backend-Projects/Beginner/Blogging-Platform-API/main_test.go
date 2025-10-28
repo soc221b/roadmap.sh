d0dbeda8608346c2c61c4304de1e6862a91c6af5
Ernest <soc221b.e@gmail.com>
2025-10-28 14:47:20 +0800

    test(blogging-platform-api): test strictly

--- Backend-Projects/Beginner/Blogging-Platform-API/main_test.go
+++ Backend-Projects/Beginner/Blogging-Platform-API/main_test.go
@@ -60,8 +60,8 @@ func TestApp(t *testing.T) {
 			expectContentType: "application/json",
 			expectBody:        fmt.Sprintf(`{"id":1,"title":"My First Blog Post","content":"This is the content of my first blog post.","category":"Technology","tags":["Tech","Programming"],"created_at":"%s","updated_at":"%s"}`, "2021-09-01T12:00:00Z", "2021-09-01T12:00:00Z"),
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectExec("INSERT INTO posts").WithArgs("My First Blog Post", "This is the content of my first blog post.", "Technology", "Tech Programming").WillReturnResult(sqlmock.NewResult(1, 1))
-				DBMock.ExpectQuery("SELECT created_at, updated_at FROM posts WHERE id =").
+				DBMock.ExpectExec("^INSERT INTO posts \\(title, content, category, tags\\) VALUES \\(\\?, \\?, \\?, \\?\\)$").WithArgs("My First Blog Post", "This is the content of my first blog post.", "Technology", "Tech Programming").WillReturnResult(sqlmock.NewResult(1, 1))
+				DBMock.ExpectQuery("^SELECT created_at, updated_at FROM posts WHERE id = \\?$").
 					WithArgs(1).WillReturnRows(
 					sqlmock.NewRows([]string{"created_at", "updated_at"}).
 						AddRow("2021-09-01 12:00:00", "2021-09-01 12:00:00"),
@@ -88,10 +88,10 @@ func TestApp(t *testing.T) {
 			expectContentType: "application/json",
 			expectBody:        fmt.Sprintf(`{"id":1,"title":"My Updated Blog Post","content":"This is the updated content of my first blog post.","category":"Tech","tags":["Programming","Tech"],"created_at":"%s","updated_at":"%s"}`, "2021-09-01T12:00:00Z", "2021-09-01T12:30:00Z"),
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectExec("UPDATE posts SET title = \\?, content = \\?, category = \\?, tags = \\? WHERE id = \\?").
+				DBMock.ExpectExec("^UPDATE posts SET title = \\?, content = \\?, category = \\?, tags = \\? WHERE id = \\?$").
 					WithArgs("My Updated Blog Post", "This is the updated content of my first blog post.", "Tech", "Programming Tech", 1).
 					WillReturnResult(sqlmock.NewResult(0, 1))
-				DBMock.ExpectQuery("SELECT id, created_at, updated_at FROM posts WHERE id =").
+				DBMock.ExpectQuery("^SELECT id, created_at, updated_at FROM posts WHERE id = \\?$").
 					WithArgs(1).WillReturnRows(
 					sqlmock.NewRows([]string{"id", "created_at", "updated_at"}).
 						AddRow(1, "2021-09-01 12:00:00", "2021-09-01 12:30:00"),
@@ -134,7 +134,7 @@ func TestApp(t *testing.T) {
 			},
 			expectStatus: 404,
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectExec("UPDATE posts SET title = \\?, content = \\?, category = \\?, tags = \\? WHERE id = \\?").
+				DBMock.ExpectExec("^UPDATE posts SET title = \\?, content = \\?, category = \\?, tags = \\? WHERE id = \\?$").
 					WithArgs("My Updated Blog Post", "This is the updated content of my first blog post.", "Tech", "Programming Tech", 1).
 					WillReturnResult(sqlmock.NewResult(0, 0))
 			},
@@ -146,7 +146,7 @@ func TestApp(t *testing.T) {
 			url:          "/posts/1",
 			expectStatus: 204,
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectExec("DELETE FROM posts WHERE id = ?").
+				DBMock.ExpectExec("^DELETE FROM posts WHERE id = \\?$").
 					WithArgs(1).
 					WillReturnResult(sqlmock.NewResult(0, 1))
 			},
@@ -165,7 +165,7 @@ func TestApp(t *testing.T) {
 			url:          "/posts/1",
 			expectStatus: 404,
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectExec("DELETE FROM posts WHERE id = ?").
+				DBMock.ExpectExec("^DELETE FROM posts WHERE id = \\?$").
 					WithArgs(1).
 					WillReturnResult(sqlmock.NewResult(0, 0))
 			},
@@ -179,7 +179,7 @@ func TestApp(t *testing.T) {
 			expectContentType: "application/json",
 			expectBody:        fmt.Sprintf(`{"id":1,"title":"My First Blog Post","content":"This is the content of my first blog post.","category":"Technology","tags":["Tech","Programming"],"created_at":"%s","updated_at":"%s"}`, "2021-09-01T12:00:00Z", "2021-09-01T12:30:00Z"),
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectQuery("SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE id =").
+				DBMock.ExpectQuery("^SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE id = \\?$").
 					WithArgs(1).WillReturnRows(
 					sqlmock.NewRows([]string{"id", "title", "content", "category", "tags", "created_at", "updated_at"}).
 						AddRow(1, "My First Blog Post", "This is the content of my first blog post.", "Technology", "Tech Programming", "2021-09-01 12:00:00", "2021-09-01 12:30:00"),
@@ -200,7 +200,7 @@ func TestApp(t *testing.T) {
 			url:          "/posts/1",
 			expectStatus: 404,
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectQuery("SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE id = ?").
+				DBMock.ExpectQuery("^SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE id = \\?$").
 					WithArgs(1).
 					WillReturnError(sql.ErrNoRows)
 			},
@@ -214,7 +214,7 @@ func TestApp(t *testing.T) {
 			expectContentType: "application/json",
 			expectBody:        fmt.Sprintf(`[{"id":1,"title":"My First Blog Post","content":"This is the content of my first blog post.","category":"Technology","tags":["Tech","Programming"],"created_at":"%s","updated_at":"%s"},{"id":2,"title":"My Second Blog Post","content":"This is the content of my second blog post.","category":"Technology","tags":["Tech","Programming"],"created_at":"%s","updated_at":"%s"}]`, "2021-09-01T12:00:00Z", "2021-09-01T12:00:00Z", "2021-09-01T12:00:00Z", "2021-09-01T12:30:00Z"),
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectQuery("SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE title LIKE \\? OR category LIKE \\? OR tags LIKE \\?").
+				DBMock.ExpectQuery("^SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE title LIKE \\? OR category LIKE \\? OR tags LIKE \\?$").
 					WithArgs("%%", "%%", "%%").WillReturnRows(
 					sqlmock.NewRows([]string{"id", "title", "content", "category", "tags", "created_at", "updated_at"}).
 						AddRow(1, "My First Blog Post", "This is the content of my first blog post.", "Technology", "Tech Programming", "2021-09-01 12:00:00", "2021-09-01 12:00:00").
@@ -231,7 +231,7 @@ func TestApp(t *testing.T) {
 			expectContentType: "application/json",
 			expectBody:        fmt.Sprintf(`[{"id":2,"title":"My Second Blog Post","content":"This is the content of my second blog post.","category":"Technology","tags":["Tech","Programming"],"created_at":"%s","updated_at":"%s"}]`, "2021-09-01T12:00:00Z", "2021-09-01T12:00:00Z"),
 			expectSql: func(DBMock sqlmock.Sqlmock) {
-				DBMock.ExpectQuery("SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE title LIKE \\? OR category LIKE \\? OR tags LIKE \\?").
+				DBMock.ExpectQuery("^SELECT id, title, content, category, tags, created_at, updated_at FROM posts WHERE title LIKE \\? OR category LIKE \\? OR tags LIKE \\?$").
 					WithArgs("%Second%", "%Second%", "%Second%").WillReturnRows(
 					sqlmock.NewRows([]string{"id", "title", "content", "category", "tags", "created_at", "updated_at"}).
 						AddRow(2, "My Second Blog Post", "This is the content of my second blog post.", "Technology", "Tech Programming", "2021-09-01 12:00:00", "2021-09-01 12:00:00"),
