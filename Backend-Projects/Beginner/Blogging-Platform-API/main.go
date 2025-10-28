87d9a06344c9d693c8552afb74d650e68eb787c1
Ernest <soc221b.e@gmail.com>
2025-10-28 11:53:15 +0800

    fix(blogging-platform-api): unable to resolve func

--- Backend-Projects/Beginner/Blogging-Platform-API/main.go
+++ Backend-Projects/Beginner/Blogging-Platform-API/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -192,4 +193,18 @@ func GetAllHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(posts)
+}
+
+func getPathValueInt(r *http.Request, w http.ResponseWriter, name string) (int, error) {
+	value, err := strconv.Atoi(r.PathValue(name))
+	if err != nil {
+		if errors.Is(err, strconv.ErrSyntax) {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return 0, err
+	}
+	return value, err
 }
